34eb7e3a1c5fcab271ed657cf22eac10dc002624
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-12-09 17:24:25 -0300

    arreglando estilos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,11 @@ import Cards from "../Cards/Cards";
 const Home = (props) => {
     return (
         <div>
-            <Cards characters={props.characters} onClose={props.onClose}/>
+            <Cards characters={props.characters} onClose={props.onClose} />
+            <h3>Para poder buscar personajes: </h3>
+            <p>Puedes buscar personajes simplemente buscandolos por sus números de ID, puedes intentar
+                escribiendo por ejemplo el número 1, el 2, etc. Mientras lo haces el personaje respectivo ira apareciendo y tendrás la posibilidad de ver su información y también de quitarlo en caso que ya no lo quieras con "close"
+            </p>
         </div>
     )
 }
