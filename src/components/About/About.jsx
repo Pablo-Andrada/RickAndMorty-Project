24e205987273d1486ed97a6298f921d0ea3f914b
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-05-25 15:05:50 -0300

    Listo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import style from "./About.module.css";
 
 const About = () => {
     return (
-        <div className={style.div} >
-            <h1>Soy PABLO ANDRADA</h1>
-            <h4 className="fst-italic">Gracias por interesarte en mi página, te cuento un poco sobre mi... </h4>
-            <h4>Soy egresado de HENRY, soy FullStack Developer, tengo conocimientos de React, NodeJS, JavaScript,</h4>
-            <h4>Express, Sequelize, Postgress, HTML y CSS.</h4>
-            <h4>Soy un programador que se interesa tanto por el front end como el back end, soy buen trabajador de equipo,</h4>
-            <h4>y me adapto rápidamente a los procesos y requerimientos del mercado actual.</h4>
-            <h4>Me encanta programar y puedo aportar soluciones en los stacks de tecnologias en los que me especializo.</h4>
-            <hr />
-            
+        <div className={style.aboutContainer}>
+            <div className={style.contentCard}>
+                <h1 className={style.mainTitle}>PABLO ANDRADA</h1>
+                
+                <div className={style.aboutText}>
+                    <p>
+                        <em>Gracias por interesarte en mi página, te cuento un poco sobre mí...</em>
+                    </p>
+                    
+                    <p>
+                        Soy egresado de HENRY, FullStack Developer con conocimientos en React, NodeJS, JavaScript, 
+                        Express, PostgreSQL, HTML y CSS.
+                    </p>
+
+                    <p>
+                        Como programador me interesa tanto el front-end como el back-end. Destaco por ser buen 
+                        compañero de equipo y adaptarme rápidamente a los procesos del mercado actual.
+                    </p>
+
+                    <h4>Tecnologías principales:</h4>
+                    <div className={style.techGrid}>
+                        {['React', 'Node.js', 'JavaScript', 'Express', 'PostgreSQL', 'HTML5', 'CSS3'].map((tech) => (
+                            <div key={tech} className={style.techItem}>
+                                {tech}
+                            </div>
+                        ))}
+                    </div>
+
+                    <h4>Mis habilidades:</h4>
+                    <div className={style.techGrid}>
+                        <div className={style.techItem}>Desarrollo FullStack integral</div>
+                        <div className={style.techItem}>Trabajo en equipo ágil</div>
+                        <div className={style.techItem}>Adaptación a nuevos desafíos</div>
+                        <div className={style.techItem}>Soluciones escalables</div>
+                    </div>
+
+                    <p>
+                        Mi motivación principal es crear software de calidad que genere impacto real, 
+                        siempre buscando mantenerme actualizado con las últimas tecnologías.
+                    </p>
+                </div>
+
+                <div className={style.homeBtnContainer}>
+                    <Link to="/home">
+                        <button className={style.homeBtn}>VOLVER AL INICIO</button>
+                    </Link>
+                </div>
+            </div>
         </div>
-    )    
+    )
 }
 
 export default About;
